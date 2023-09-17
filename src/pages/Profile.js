5341be82478d2fcebd9c18987220d0c7c808d14d
nikhil naroliya <nikhilnaroliya7exams@gmail.com>
2023-09-17 11:56:56 +0530

    added source code link to profile

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -9,10 +9,16 @@ export function Profile() {
 
        <p id="my-description" style={{}}> Hi Iam <>Nikhil Kumar Naroliya</>  a MCA final year student at NIT Agartal ,for  help regarding studies you can contact me through given chhanels .  </p>
        <span style={{color:"white"}}>nikhilnaroliya6953@gmail.com</span>
+       
+       <br></br>
+       <br></br>
+       <span ><Link id="linkedinbtn"  style={{cursor:"pointer",textDecoration:"none",color:"white",border:"1px solid white",borderRadius:"0.5rem",padding:"0.2rem"}} to="https://github.com/NikhilNaroliya/Study-Help" >Source Code</Link></span>
        <br></br>
        <br></br>
    
        <Link id="linkedinbtn" style={{color:"white" ,border:"1px solid white", borderRadius:"0.5rem",padding:"0.5rem",textDecoration:"none"}} to="https://www.linkedin.com/in/nikhil-naroliya-a3b2a8238">LinkedIn</Link>
+
+       
  
       </div>
     );
